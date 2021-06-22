0ce24d9cd77bb4f6fd0972995ccf1dc01c01656f
Michal Siminski <michal.siminski3@gmail.com>
2021-06-22 10:13:45 +0200

    ngfor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,39 @@
 import { Component } from '@angular/core';
+import { Task } from './task';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  config: { [key: string]: string | Date } = null;
+
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2021-01-02',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie',
+      deadline: '2020-03-02',
+      done: false,
+    },
+  ];
+
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista zadań',
+        footer: 'Lista zadań zbudowana w Angularze',
+        date: new Date(),
+      };
+    }, 500);
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import localePl from '@angular/common/locales/pl';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePl);
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
